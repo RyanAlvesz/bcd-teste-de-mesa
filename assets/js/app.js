e36e56e6cc249b1874cc748ec89fa6cb07595c2a
RyanAlvesz <ra546038@gmail.com>
2023-12-03 03:43:05 -0300

    finalizando versao desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,145 @@
 'use strict'
 
-let botaoAdicionarLinha = document.getElementById('adicionar-linha')
-let id = 5
+let botaoAdicionarLinha = document.getElementsByClassName('adicionar-linha')
+let botaoRemoverLinha = document.getElementsByClassName('remover-linha')
+let botaoAdicionarColuna = document.getElementsByClassName('adicionar-coluna')
+let botaoRemoverColuna = document.getElementsByClassName('remover-coluna')
+let botaoReset = document.getElementById('reset')
 
-botaoAdicionarLinha.addEventListener('click', () => {
 
-    let tabela = document.getElementById('t3')    
-    let linha = document.createElement('tr')
-    let td1 = criarColuna(id, 'span')
-    id++    
-    let td2 = criarColuna('', 'input')
-    let td3 = criarColuna('', 'input')
-    linha.replaceChildren(td1, td2, td3)
-    tabela.appendChild(linha)
+for(let botao of botaoAdicionarLinha){
+
+    botao.addEventListener('click', () => {
+
+        let qntColunas = botao.parentNode.parentNode.parentNode.children[1].children.length
+        let qntLinhas = botao.parentNode.parentNode.parentNode.children.length
+        let idFinal = botao.parentNode.parentNode.parentNode.children[qntLinhas - 5].children[0].textContent
+        let id = Number(idFinal)
+
+        let tabela = document.getElementById(`t${botao.classList[1]}`)
+        let linha = document.createElement('tr')
+        let td1 = criarColuna(id + 1, 'span')
+        linha.appendChild(td1)
+
+        for(let i = 1; i < qntColunas; i++){
+            let td = criarColuna('', 'input') 
+            linha.appendChild(td)
+        }
+
+        tabela.children.item(id + 2 ).insertAdjacentElement('beforeBegin',linha)
+
+    })
+
+}
+
+for(let botao of botaoRemoverLinha){
+
+    botao.addEventListener('click', removerLinha => {
+
+        let qntLinhas = botao.parentNode.parentNode.parentNode.children.length
+        let idFinal = botao.parentNode.parentNode.parentNode.children[qntLinhas - 5].children[0].textContent
+        let id = Number(idFinal)
+
+        if(id != 4){
+            let tabela = document.getElementById(`t${botao.classList[1]}`)    
+            let linhaFinal = tabela.children[id + 1]
+            tabela.removeChild(linhaFinal)
+        }
+
+    })
+
+}
+
+for(let botao of botaoAdicionarColuna){
+
+    botao.addEventListener('click', () => {
+
+        for(let linha of botao.parentNode.parentNode.parentNode.children){
+                
+            let qntColunas = botao.parentNode.parentNode.parentNode.children[1].children.length
+        
+            if(linha.children[0].colSpan == 1 && !linha.classList[0]){
+                
+                let coluna = criarColuna('', 'input')
+                linha.appendChild(coluna)
+
+            }else{
+
+                linha.children[0].colSpan = Number(qntColunas) + 1
+            
+            }
+
+        }
+
+    })
+
+}
+
+for(let botao of botaoRemoverColuna){
+
+    botao.addEventListener('click', () => {
+
+        let qntColunas = botao.parentNode.parentNode.parentNode.children[1].children.length
+        
+        for(let linha of botao.parentNode.parentNode.parentNode.children){    
+        
+            if(qntColunas != 3 && linha.children[0].colSpan == 1 && !linha.classList[0]){
+                let colunaFinal = linha.children[linha.children.length - 1]  
+                linha.removeChild(colunaFinal)
+            }
+
+        }
+
+    })
+
+}
+
+botaoReset.addEventListener('click', () => {
+
+    let tabelas = document.getElementsByClassName('tabela')
+    
+    for(let tabela of tabelas){
+        
+        let qntLinhas = tabela.children.length
+        let idFinal = tabela.children[qntLinhas - 5].children[0].textContent
+        let id = Number(idFinal)
+
+        if(id != 4){
+
+            for(let i = id; i > 4; i--){
+
+                let linhaFinal = tabela.children[i + 1]
+                tabela.removeChild(linhaFinal)
+
+            }
+
+        } 
+
+        let qntColunas = tabela.children[1].children.length
+        
+        for(let linha of tabela.children){    
+            
+            if(qntColunas != 3 && linha.children[0].colSpan == 1 && !linha.classList[0]){
+                
+                for(let i = qntColunas; i > 3; i--){
+
+                    let colunaFinal = linha.children[i - 1]  
+                    linha.removeChild(colunaFinal)
+
+                }
+
+            }
+
+        }
+
+    }
 
 })
 
-const criarColuna = (conteudoUsuario, tipoUsuario) => {
+const criarColuna = (conteudoParametro, tipoParametro) => {
 
-    let conteudo = conteudoUsuario
-    let tipo = tipoUsuario
+    let conteudo = conteudoParametro
+    let tipo = tipoParametro
 
     const td = document.createElement('td')
 
@@ -36,3 +156,4 @@ const criarColuna = (conteudoUsuario, tipoUsuario) => {
     return td 
 
 }
+
